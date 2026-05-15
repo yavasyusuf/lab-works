9fb9daee795d73a7151d012f47432bfa84364c4e
yavasyusuf <yavasyusuf2005@gmail.com>
2026-05-15 12:22:10 +0300

    A system collects daily temperatures for a week
    Write a C program that:
    1. Finds the maximum temperature
    2. Replaces that maximum value with -1
    3. Performs all operations inside a function using pointers

---
+++ lab4.c.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define SIZE 7
+void replaceMin(int *temp){
+	printf("Enter 7 temperature values: ");
+		int min = *temp;
+		int i;
+		scanf("%d", temp);  //u should give first value unless it could be garbage value
+	for(i = 1;i<SIZE;i++){
+		scanf("%d", temp + i);
+		
+		 min = *temp;
+		
+		if(*(temp+i) < min){
+			min = *(temp + i);
+		}
+		
+		}
+	for(i = 0;i<SIZE;i++){
+		if(*(temp + i) == min)
+		{
+			 *(temp + i) = -1;
+		}
+	}
+}
+
+ int main(){
+   int temp[SIZE];
+   int i;
+     
+     replaceMin(temp);
+      printf("Updated temperatures:\n");
+      
+       for(i = 0;i<SIZE;i++){
+       	printf(" %d",*(temp + i));
+	   }
+   return 0;
+ }
